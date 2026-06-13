265d2b633b4e0dcbba7916236bd77fc79acbcdb1
Sushil338 <sushiltanwar338@gmail.com>
2026-06-14 00:31:54 +0530

    Changed URL

--- src/main/java/com/manager/TODO/Controllers/AiController.java
+++ src/main/java/com/manager/TODO/Controllers/AiController.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/ai")
-@CrossOrigin(origins = "http://localhost:5173")
 public class AiController {
 
     private final AiTaskService aiTaskService;
--- src/main/java/com/manager/TODO/Controllers/TaskController.java
+++ src/main/java/com/manager/TODO/Controllers/TaskController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
